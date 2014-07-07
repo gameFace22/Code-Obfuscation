31e2447574d2edd9096f239454ff993394f40ff0
Nishaanth Gunasekaran <nishaanth.guna@gmail.com>
2014-07-07 06:45:42 +0000

    Create filethree.c

---
+++ filethree.c
@@ -0,0 +1,26 @@
+						#include <stdio.h>
+					#define O_ (
+				#define _O )
+			#define o_o 1
+				#define O_o printf
+						#define O_O
+							#define o_O ;
+					   #define _0 while   
+							#define _oo main
+									#define _OO int
+
+	O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O
+	  O_O	O_O  _oo O_O	 O_ _O    O_O O_O{	O_O _____ O_O	 O_O	O_O   O_O  O_O O_O    O_O  O_O
+	    O_O	  O_ O_O O_O  O_O    O_O  O_O - o_o,  O_O o_o,  O_O 0 O_O _O O_O o_O}  O_O  O_O  O_O  O_O o_O    O_O
+	     O_O  O_O O_O  O_O _____ O_O O_O  O_  O_O _,   O_O __ O_O, O_O ___ O_O _O	 O_O {    O_O   O_O   O_O  O_O
+ 	   O_O	_OO  O_O  O_O ______ O_O = O_O o_o O_O o_O O_O _0 O_O O_ O_O  ______ O_O != O_O 11 O_O _O O_O{  O_O
+	 O_O	O_O ___ O_O = O_O _ O_O + O_O __ O_O o_O O_O _ O_O = O_O __ o_O O_O __ O_O = O_O ___ o_O    O_O
+           O_O  O_O O_o O_O O_ O_O  "%d\n" ,O_O  ___  O_O _O O_O o_O O_O ______ O_O ++ O_O o_O O_O }} O_O  O_O
+	O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O O_O
+
+
+
+
+
+
+
