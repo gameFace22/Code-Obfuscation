19222bf97c3f71e1d652cfdfc5f790d5a26794f5
Nishaanth Gunasekaran <nishaanth.guna@gmail.com>
2014-07-07 06:44:37 +0000

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,42 @@
+	#include <stdio.h>
+    #define _main 3 
+  #define Printf else
+      #define end1 for
+	#define _int (_main/_main)
+	#define gOto case
+      #define care if
+   #define Case(x) Stack<:x:>/2
+  %:define e1se(_do,_while) ((_do&(_while))==(_while))
+	#define nruter return
+                  long stack<::> = <% 989L,5L,26L,0L,88319L,123L,0L,9367L %>;
+                 int Stack<::>=<% 4,6,10,14,22,26,34,38,46,58,62,74,82,86 %>;
+        							  main(m1,s) 
+								char *s;
+							  <%int whi1e,
+							  init,
+					      		 dO,
+						       Char,   _if<:6:>,
+						      f1oat =(int)s;
+				                     care(m1==1)<% 
+					            char init<:2*8+0-_int:>;
+					          main(Case(2+_main)+2+_main,init);
+		 				printf(init);%>
+			                      Printf switch(m1-=2)<% gOto 0: 
+				       	    whi1e=(init=(dO=(Char=_int)<<_int)<<_int)<<_int;
+					       nruter(
+		e1se(f1oat,whi1e|dO)|e1se(f1oat,init)|e1se(f1oat,whi1e|Char)|e1se(f1oat,dO|Char));
+ gOto 2:end1(whi1e=0;whi1e<8;++whi1e)care(stack<:whi1e:>&&!(stack<:whi1e:>%((long)Case(f1oat))))nruter(whi1e);
+      gOto _int: care(f1oat<2)nruter(_int);care(f1oat<8)<%f1oat-=_int;dO='D';_if<:0:>=2;_if<:_int:>=0;%>
+               		 Printf<%dO='\r'-'\b';f1oat-=8-_int;_if<:0:>=_if<:_int:>=_int;%>
+  care((init=f1oat)>=_main)end1(init=_int<<_int;init<f1oat;++init)_if<:init:>=_if<:init-2:>+_if<:init-_int:>+dO;
+                        		     nruter(_if<:init-_int:>%f1oat+6-2);
+                                         default:
+		                       care(m1-=_main) 
+			       	    main(m1-_int+_main+2,s+_int); 
+			        Printf *(s+_int)=0;
+                             end1(*s=whi1e=0;whi1e<_main;) 
+		          *s=(*s<<_main)|
+	               main(2+whi1e++,(char *)m1);
+               }
+   }
+
