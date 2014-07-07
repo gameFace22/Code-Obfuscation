af0447885fd441184e1f0830f4e8e0a8494b085f
Nishaanth Gunasekaran <nishaanth.guna@gmail.com>
2014-07-07 06:43:53 +0000

    Create fileone.c

---
+++ fileone.c
@@ -0,0 +1,34 @@
+          								  #include<stdio.h> 
+						       #define whi1e int
+								#define _else
+							                  #define _if  (
+									           #define _esle  )
+									   #include<string.h>
+									        whi1e _l1 _if 
+								       	  whi1e _l  _else  _esle
+								    _else{ _else  return _l == 0 
+							   ?_else _else 0: _else  _l  _else % _else 10 
+							 _else + _else _l1 _else _else  _if _l/10  _esle ;
+						     _else ; _else } main  _else _else  _if  _else   _esle  {  
+						  _else	whi1e   _else	_0 = _else _else	_else     12; char 
+					     _else   O_ <:13:>;  _else; char  _else  _else l01= '\065'; _else; _else
+								  		   whi1e _else _l
+								  		     = _else 143;  	
+								  			strcpy 
+								  			_if O_,
+					     "\x39\066\x35\067\045\x46\127\x59\130\x46\106\x4D\130"_else 
+				 	_esle ; _else;	lOl:if _if _l!=268 _esle	 <% _else printf  _if "%c", _if 
+				 	         _0 _else <: _else O_ _else:> _else - _if _else  _if _else  _if  
+				 	     	   _else   _else    whi1e   _else _else   _esle  _else    _else 
+				 	     	     	 l01   _esle _else -48  _else   _esle  _else  _else 
+				 	     	     	       _esle _else  _else     _esle ; _else  _l
+				 	     	     	          	 = _else _l _else + _else _l1 
+				 	     	     	           		_else   _else _if _else
+				 	     	     	           		    _l  _esle ;   _else
+				 	     	     	           		         _0--;   _else
+				 	     	     	           		              goto lOl
+				 	     	     	           		              ;_else }}
+
+
+
+
